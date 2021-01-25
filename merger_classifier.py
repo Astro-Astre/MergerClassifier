5aaa532c75423a101c94a711ddc70565b9a4ed80
astre <908112836@qq.com>
2021-01-25 19:40:16 +0800

    code

--- merger_classifier.py
+++ merger_classifier.py
@@ -3,6 +3,7 @@ import datetime
 import os
 import pickle
 import time
+from multiprocessing import Pool, pool
 
 import numpy as np
 import torch
@@ -10,19 +11,46 @@ from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
+
 import fscore
-from multiprocessing import Pool, pool
+
 # 配置参数
-torch.manual_seed(1)  # 设置随机数种子，确保结果可重复
-BATCH_SIZE = 64   # 批处理大小
-learning_rate = 1e-3  # 学习率
-num_epoches = 50  # 训练次数
+torch.manual_seed(1)    # 设置随机数种子，确保结果可重复
+BATCH_SIZE = 64 # 批处理大小
+LEARNING_RATE = 1e-3    # 学习率
+EPOCHES = 50    # 训练次数
+DATA_SOURCE_DIR = r"D:\Code\MachineLearning\Data\2020.12.15_MergerClassifier"   # 总数据存放文件夹
+MODEL_SOURCE_DIR = r"D:\Code\MachineLearning\Model\2020.12.15_MergerClassifier" # 总模型存放文件夹
+
+def changeAxis(data):
+    '''
+    输入即将转换为张量之前的ndarray,要求为C×H×W
+    由于ToTensor会将nadarrayHWC转为CHW
+    要先把ndarray的CHW转为ndarray的HWC.
+    '''
+    img = np.swapaxes(data, 0, 2)
+    img = np.swapaxes(img, 0, 1)
+    return img
+
 def dataPrepare(file, transform):
     img = pickle.load(file)
-    img = np.swapaxes(img, 0, 2)
-    img = np.swapaxes(img, 0, 1) # 因为ToTensor会将nadarrayHWC转为CHW，所以要先把ndarray的CHW转为ndarray的HWC
-    img = transform(img) # 数据标签转换为Tensor
+    img = changeAxis(img)
+    img = transform(img)    # 数据标签转换为Tensor
     return img
+
+def createPackage(model_package):
+    if not os.path.isdir(model_package):
+            os.mkdir(model_package)
+
+def modelPackageWrite(info):
+    '''
+    返回文件夹名：model_info_datetime
+    '''
+    model_package = 'model_%s'%(info)
+    if not model_package[-1] =='_':
+        model_package+='_'
+    model_package += str(datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S'))
+    return model_package
 
 class MerGalDataset(Dataset):
     '''
@@ -125,52 +153,56 @@ def predict(path,model_name):
             _, predicted = torch.max(outputs, 1)
             return predicted[0]
 
-def trainModel(model_package, RESUME, start_epoch):
+def trainModel(model_package, continue, last_epoch):
     '''
     训练
     '''
-    if not os.path.isdir('%s' %(model_package)):
-        os.makedirs('%s' %(model_package))
+    createPackage('%s' %(model_package))
+
     writer = SummaryWriter(model_package)
+
     loss_func = nn.CrossEntropyLoss() #损失函数：交叉熵
-    optimizer = optim.SGD(model.parameters(), lr= learning_rate, momentum= 0.9) # 优化器
+    optimizer = optim.SGD(model.parameters(), lr= LEARNING_RATE, momentum= 0.9) # 优化器
     losses = []
     acces = []
     eval_losses = []
     eval_acces = []
     start = -1
-    # RESUME = True时进行断点续训
-    if RESUME:
-        path_checkpoint = '%s\checkpoint\ckpt_best_%d.pth' %(model_package, start_epoch)  # 断点路径
+    
+    # continue = True时进行断点续训
+    if continue:
+        path_checkpoint = '%s\%s\checkpoint\ckpt_best_%d.pth' %(MODEL_SOURCE_DIR, model_package, last_epoch)  # 断点路径
         checkpoint = torch.load(path_checkpoint)  # 加载断点
         model.load_state_dict(checkpoint['net'])  # 加载模型可学习参数
         optimizer.load_state_dict(checkpoint['optimizer'])  # 加载优化器参数
-        start = checkpoint['epoch']  # 设置开始的epoch
-        print('start epoch: ', start_epoch + 1)
+        start = checkpoint['epoch']  # 读取上次的epoch
+        print('start epoch: ', last_epoch + 1)
+
     # 保存训练记录
     train_log = open(model_package + '//train_log.txt','a')
     train_log.write(str(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
-    train_log.write('\nBATCH_SIZE = %d\nlearning_rate = %f\n\n' %(BATCH_SIZE, learning_rate))
+    train_log.write('\nBATCH_SIZE = %d\nLEARNING_RATE = %f\n\n' %(BATCH_SIZE, LEARNING_RATE))
     train_log.close()
+
     # 开始训练，带writer的都是tensorboard可视化的代码
-    for echo in range(start + 1, num_epoches):
+    for echo in range(start + 1, EPOCHES):
         train_log = open(model_package + '//train_log.txt','a')
-        train_loss = 0   # 训练损失
-        train_acc = 0    # 训练准确度
+        train_loss = 0  # 训练损失
+        train_acc = 0   # 训练准确度
         model.train()
         tn = 0
         tp = 0
         fn = 0
         fp = 0
-        for i, (X, label) in enumerate(train_loader):     # 遍历train_loader
+        for i, (X, label) in enumerate(train_loader):   # 遍历train_loader
             label = torch.as_tensor(label, dtype = torch.long)
             X, label = X.to(device), label.to(device)
-            out, conv1, conv2, conv3, conv4 = model(X)      # 正向传播
+            out, conv1, conv2, conv3, conv4 = model(X)  # 正向传播
             lossvalue = loss_func(out, label)    # 求损失值
-            optimizer.zero_grad()               # 优化器梯度归零
-            lossvalue.backward()                # 反向转播，刷新梯度值
-            optimizer.step()                    # 优化器运行一步，注意optimizer搜集的是model的参数
-            train_loss += float(lossvalue)      # 计算损失
+            optimizer.zero_grad()   # 优化器梯度归零
+            lossvalue.backward()    # 反向转播，刷新梯度值
+            optimizer.step()    # 优化器运行一步，注意optimizer搜集的是model的参数
+            train_loss += float(lossvalue)  # 计算损失
             _, pred = out.max(1)
             num_correct = (pred == label).sum()
             for i in range(len(label)):
@@ -188,6 +220,7 @@ def trainModel(model_package, RESUME, start_epoch):
             writer.add_image('conv2_image', conv2, i, dataformats= 'HW')
             writer.add_image('conv3_image', conv3, i, dataformats= 'HW')
             writer.add_image('conv4_image', conv4, i, dataformats= 'HW')
+
         fscore.caculate(tp,tn,fp,fn)
         writer.add_scalar('loss', train_loss, echo)
         losses.append(train_loss / len(train_loader))
@@ -203,7 +236,7 @@ def trainModel(model_package, RESUME, start_epoch):
 
         eval_loss = 0
         eval_acc = 0
-        model.eval() # 模型转化为评估模式
+        model.eval()    # 模型转化为评估模式
         tn = 0
         tp = 0
         fn = 0
@@ -242,8 +275,7 @@ def trainModel(model_package, RESUME, start_epoch):
             'optimizer':optimizer.state_dict(),
             "epoch": echo
         }
-        if not os.path.isdir('%s/checkpoint' %(model_package)):
-            os.mkdir('%s/checkpoint' %(model_package))
+        createPackage('%s/checkpoint' %(model_package))
         torch.save(checkpoint, '%s/checkpoint/ckpt_best_%s.pth' %(model_package,str(echo)))
         torch.save(model, '%s/model_%d.model' %(model_package,echo))
         writer.close()
@@ -260,37 +292,24 @@ def verifyModel(path, label, model_name):
     if label == 0:
         print('Galaxy验证集准确率：', acc_num*100/len(pre), '%')
 
-def modelPackageWrite():
-    model_package = 'model_normal'
-    model_package += '-'
-    model_package += str(datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S'))
-    return model_package
+
 
 def verify(path,model):
     verifyModel(path+'\\0', 0, model)
     verifyModel(path+'\\1', 1, model)
 
 if __name__ == "__main__":
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
     start = time.time()
     model = Model()
     device = torch.device("cuda:0")
     model.to(device)
-    model_package_name = 'model//' + modelPackageWrite()
+    model_package_name = '%s//'%(MODEL_SOURCE_DIR) + modelPackageWrite('normal_')
     # 模型保存文件夹（无需实现创建），是否断点续训，如果断点续寻，上次训练到第几个epoch了
     # model_package_name = r'model\model_normal-2021-01-03-085331'
-    # trainModel(model_package_name, RESUME=True, start_epoch= 8)
+    trainModel(model_package_name, continue=True, last_epoch= 8)
 
-    # 验证：验证集数据路径，真实标签，模型名
-    # p=Pool(4)
-    # verify_data = r'classifier_data\verify_data'
-    # p.apply_async(verify,args=(verify_data, r'model\model_normal-2021-01-03-085331\model_6.model',))
-    # p.apply_async(verify,args=(verify_data, r'model\model_normal-2021-01-03-085331\model_9.model',))
-    # p.apply_async(verify,args=(verify_data, r'model\model_normal-2021-01-03-085331\model_21.model',))
-    # p.apply_async(verify,args=(verify_data, r'model\model_normal-2021-01-03-085331\model_22.model',))
-    # p.apply_async(verify,args=(verify_data, r'model\model_normal-2021-01-03-085331\model_23.model',))
-    # p.close()
-    # p.join()
-    verifyModel(r'classifier_data\verify_data\\0', 0, r'model\model_normal-2021-01-01-014012\model_48.model')
-    verifyModel(r'classifier_data\verify_data\\1', 1, r'model\model_normal-2021-01-01-014012\model_48.model')
+    # verifyModel(r'classifier_data\verify_data\\0', 0, r'model\model_normal-2021-01-01-014012\model_48.model')
+    # verifyModel(r'classifier_data\verify_data\\1', 1, r'model\model_normal-2021-01-01-014012\model_48.model')
     endtime = time.time()
     print('time cost:',endtime-start)
