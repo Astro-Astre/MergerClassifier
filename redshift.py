e38b070776cd1bebf7a5293028e89da14956cfdc
astre <908112836@qq.com>
2021-01-26 15:55:09 +0800

    code

--- redshift.py
+++ redshift.py
@@ -4,14 +4,18 @@ import sys
 import time
 import identify as it
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 if __name__ == "__main__":
     start = time.time()
-    f = open(r'galaxy.txt','r')
+    f = open(r'merger.txt','r')
     data= f.readlines()
     f.close()
+    x = []
     for i in range(len(data)):
         percent = 1.0 * i / len(data)  #用于显示进度
+
         img_name, label = [str(j) for j in data[i].split()]
         img_name = img_name.split('_')[0]+'.jpg'
         img_dir =os.path.join(r'raw_data\jpg\redshift_galaxy_jpg',img_name)
@@ -22,12 +26,14 @@ if __name__ == "__main__":
             print(img_dir)
         else:
             if label=='0':
-                plt.scatter(i,y,c='r',marker='.')
-            else :
-                plt.scatter(i,y,c='b',marker='.')
+            #     plt.scatter(i,y,c='r',marker='.')
+            # else :
+            #     plt.scatter(i,y,c='b',marker='.')
+                x.append(y)
             sys.stdout.write("进度：%.4f"%(percent*100))
             sys.stdout.write("%\r")
             sys.stdout.flush()
     endtime = time.time()
     print('time cost:',endtime-start)
+    sns.histplot(x)
     plt.show()
